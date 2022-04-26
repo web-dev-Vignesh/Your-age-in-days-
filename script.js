92f65ddfebc37f8de607ef1a05ba798855ee29e2
web-dev-Vignesh <vickyrohit.2001@gmail.com>
2022-04-26 09:03:24 +0530

    someChange

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
  
 function ageInDays(){
-    var rem=document.getElementById("demo");
+    var rem=document.getElementById("rem");
     rem.remove();
     var ageyear = prompt('Enter your Brith Year');
     var year = new Date().getFullYear();
     var ageInDayss=(year-ageyear)*365;
+    var age=year-ageyear;
     // console.log(ageInDayss);
     var h1=document.createElement('h1');
-    var Total =document.createTextNode('You are '+ageInDayss+' days old');
-    h1.setAttribute('id','ageInDay');
-    h1.appendChild(Total);
+    var total=document.createTextNode('You are '+age+ ' years old.'+' You are ' + ageInDayss + ' days old');
+    h1.setAttribute('id','rem');
+    h1.appendChild(total);
     document.getElementById('flex-box-result').appendChild(h1);
-
+    
 }
 function reset(){
-    document.getElementById('ageInDay').innerText = 'Thankyou for Using this to known how old are you in days.To Continue please Refresh/Reload the page......';
      
+    document.getElementById('rem').innerText = 'Thankyou for Using this to known your age and days';
 }
